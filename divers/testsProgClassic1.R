b3526310ee4285cddcc66e563089700c5209b3fc
genolini <genolini@u-paris10.fr>
2008-07-22 00:00:00 +0000

    version0.5

---
+++ divers/testsProgClassic1.R
@@ -0,0 +1,10 @@
+##########################################
+### Nom du programme : testClassique.R ###
+##########################################
+load("../data/dataAges.rda")
+source("../R/progClassic.R")
+
+publicA(1)
+privateA(2)
+.publicB(3)
+
