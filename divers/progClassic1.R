ac26c455b8a576501ab01a969ef0a8038f608c97
Christophe Genolini <genolini@u-paris10.fr>
2009-10-15 00:00:00 +0000

    version 0.5.2

--- divers/progClassic1.R
+++ divers/progClassic1.R
@@ -4,7 +4,3 @@
 
 publicA     <- function(x){plot(x+1)}
 privateA    <- function(x){x+2}
-.publicB    <- function(x){x+10}
-.privateB   <- function(x){x+20}
-publicC     <- function(x){publicA(privateA(x))}
-privateC    <- function(x){.publicB(.privateB(x))}
--- testsDev/testsProgClassic.R
+++ testsDev/testsProgClassic.R
@@ -1,12 +1,12 @@
 #################################################################
-###testProgClassique.R###
+###                    testProgClassique.R                    ###
 #################################################################
 load("../data/dataAges.rda")
 source("../R/progClassic.R")
-print(publicA(1))
-(privateA(2))
-(.publicB(3))
-(.privateB(4))
-(publicC(5))
-(publicC(dataAges$age))
-(privateC(6))
+publicA(1)
+privateA(2)
+.publicB(3)
+.privateB(4)
+publicC(5)
+publicC(dataAges$age)
+privateC(6)
